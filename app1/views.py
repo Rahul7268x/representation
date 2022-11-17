ad624da0c193029559ca37ea4499ba81af607f93
Rahul Singh <rahulkumar2001son@gmail.com>
2022-11-17 20:27:04 +0530

    First Upload

---
+++ app1/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request,'a.html', {'form':'django','link':'https://www.google.co.in/'})
+
+
+# def Profile(request):
+#     return HttpResponse("Profiel page")
+
+def expretion(request):
+    a = int(request.POST['text1'])
+    b = int(request.POST['text2'])
+    c = a + b
+    return render(request,'output.html',{'return':c})
